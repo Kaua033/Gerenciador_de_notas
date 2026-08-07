c3bd4e2ea4a6dfc2e67dec25967cbbaf06885a86
Kaua033 <suelen.kaua16@gmail.com>
2026-08-07 00:53:12 -0300

    Initial commit: Spring Boot project

---
+++ src/main/java/gerador/denotas/DenotasApplication.java
@@ -0,0 +1,13 @@
+package gerador.denotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerador/denotas/DenotasApplicationTests.java
@@ -0,0 +1,13 @@
+package gerador.denotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
